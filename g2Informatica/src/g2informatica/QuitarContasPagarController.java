e497c83e39bc2a49f924f06cba9e5f86c1eeafc1
victor hugo <play.victor95@gmail.com>
2019-05-24 09:37:00 -0300

    talas quitar contas a pagar fieta

--- g2Informatica/src/g2informatica/QuitarContasPagarController.java
+++ g2Informatica/src/g2informatica/QuitarContasPagarController.java
@@ -7,7 +7,11 @@ package g2informatica;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +20,21 @@ import javafx.fxml.Initializable;
  */
 public class QuitarContasPagarController implements Initializable {
 
+    @FXML
+    private Button btConfirmar;
+    @FXML
+    private Button btLimpar;
+    @FXML
+    private Button btSair;
+    @FXML
+    private Button btAdicionar;
+    @FXML
+    private Button btCancelar;
+    @FXML
+    private Button btRemover;
+    @FXML
+    private TextField txValorPago;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +42,29 @@ public class QuitarContasPagarController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void evtConfirmar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtLImpar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtSair(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtAdiconar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtCancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtRemovar(ActionEvent event) {
+    }
     
 }
